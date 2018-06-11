51dcacab5d9e5fda482bba99aca085eb7c109b38
alphacrash <sudhansu.ace@gmail.com>
2018-06-12 00:36:23 +0530

    Create new post feature, Added Post Schema, Connected to MongoDB

--- app.js
+++ app.js
@@ -7,21 +7,53 @@ mongoose.connect("mongodb://localhost/an-express-blog");
 var app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var postSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    content: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Post = mongoose.model("Post", postSchema);
+
+// Index Page
 app.get("/", function (req, res) {
     res.render("index");
 });
 
-var posts = [
-    { title: "Just lying on the ground", image: "https://cdn.stocksnap.io/img-thumbs/960w/GGQVLCTSWI.jpg" },
-    { title: "Foot in sand", image: "https://cdn.stocksnap.io/img-thumbs/960w/YRI8K6RLX2.jpg" },
-    { title: "Make it happen", image: "https://cdn.stocksnap.io/img-thumbs/960w/YEW4VFXSIG.jpg" }
-];
-
+// Archives
 app.get("/posts", function (req, res) {
-    res.render("posts", { posts: posts });
+    Post.find({}, function (err, allPosts) {
+        if (err) {
+            console.log(err);
+            res.redirect("/");
+        } else {
+            res.render("posts", { posts: allPosts });
+        }
+    });
+});
+
+// Create new post
+app.get("/posts/new", function (req, res) {
+    res.render("posts/new");
+});
+
+app.post("/posts", function (req, res) {
+    var newPost = req.body.post;
+
+    Post.create(newPost, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/posts");
+        }
+    });
 });
 
 app.listen(3000, function () {
